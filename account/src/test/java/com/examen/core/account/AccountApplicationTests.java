4e2f33f169fd0d03e1d01cef2462bdd21b9436e3
Nayeli Michelle Tipantiza Cumbal <106408982+nayelimtc@users.noreply.github.com>
2024-12-14 08:22:27 -0500

    Subida de gits

---
+++ account/src/test/java/com/examen/core/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.core.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
